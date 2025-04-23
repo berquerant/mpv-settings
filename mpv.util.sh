33013e7c37d8174edcd1d379f817b894132c5715
berquerant <sin@sin-pro.local>
2025-04-23 23:47:09 +0900

    Use tmp file as tmp playlist, remove playlogs

--- mpv.util.sh
+++ mpv.util.sh
@@ -2,25 +2,6 @@
 
 export MPV_COMMON_OPTIONS=""
 
-export MPV_PLAY_HISTORY="${YTDL_LOGD}/mpv.play.history"
-# mpv_append_play_history [UUID] LOG
-# append following log to MPV_PLAY_HISTORY:
-#   DATETIME UUID LOG
-# DATETIME format is %Y-%m-%d %H:%M:%S
-# generate UUID if UUID is omitted
-mpv_append_play_history() {
-    local uid
-    if [ $# = 1 ] ; then
-        uid="$(uuidgen)"
-    else
-        uid="$1"
-        shift
-    fi
-    local now
-    now="$(date "+%Y-%m-%d %H:%M:%S")"
-    echo "${now} ${uid} $*" >> "$MPV_PLAY_HISTORY"
-}
-
 mpv_play_help() {
     cat <<EOS
 mpv_play - play music or video
@@ -60,21 +41,9 @@ mpv_music() {
         mpv ${MPV_COMMON_OPTIONS} --no-video --ytdl-format='worstvideo+bestaudio' --shuffle --playlist="$1"
         return 0
     fi
-    local finished
-    __cleanup() {
-        finished=1
-    }
-    trap __cleanup SIGINT
-    local uid
-    uid="$(uuidgen)"
-    while read -r line ; do
-        if ((finished)) ; then
-            return 1
-        fi
-        echo "$line"
-        mpv_append_play_history "$uid" "music $line"
-        mpv ${MPV_COMMON_OPTIONS} --no-video --ytdl-format='worstvideo+bestaudio' "$line"
-    done
+    local -r tmplist="$(mktemp)"
+    cat - > "$tmplist"
+    mpv ${MPV_COMMON_OPTIONS} --no-video --ytdl-format='worstvideo+bestaudio' --playlist="$tmplist"
 }
 
 mpv_video() {
@@ -82,19 +51,7 @@ mpv_video() {
         mpv ${MPV_COMMON_OPTIONS} --ontop --border=no --autofit=600 --geometry=100%:100% --ytdl-format='[height<=480]+bestaudio' --shuffle --playlist="$1"
         return 0
     fi
-    local finished
-    __cleanup() {
-        finished=1
-    }
-    trap __cleanup SIGINT
-    local uid
-    uid="$(uuidgen)"
-    while read -r line ; do
-        if ((finished)) ; then
-            return 1
-        fi
-        echo "$line"
-        mpv_append_play_history "$uid" "video $line"
-        mpv ${MPV_COMMON_OPTIONS} --ontop --border=no --autofit=600 --geometry=100%:100% --ytdl-format='[height<=480]+bestaudio' "$line"
-    done
+    local -r tmplist="$(mktemp)"
+    cat - > "$tmplist"
+    mpv ${MPV_COMMON_OPTIONS} --ontop --border=no --autofit=600 --geometry=100%:100% --ytdl-format='[height<=480]+bestaudio' --playlist="$tmplist"
 }
